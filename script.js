2bab0ddd7c931ba85dcc6c725dc618ae28785f12
Deepika Kar <deepikakar2003@gmail.com>
2023-08-18 19:55:07 +0530

    first commit nav

---
+++ script.js
@@ -0,0 +1,8 @@
+// const API_KEY = "6bb6a86e30f540b78774ca42c668a902";
+// const url = "https://newsapi.org/v2/everything?q="
+
+// window.addEventListener('load', () => fetchNews("India"));
+
+// async function fetchNews (query) {
+
+// }
